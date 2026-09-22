3260be9e40447c95e65e01da094244ac899bbdfc
kevinhsdev <kevinhs07@outlook.com>
2026-09-22 09:17:45 -0300

    remove test code

--- Terça/aula7/new_app/test/new_app_test.dart
+++ Terça/aula7/new_app/test/new_app_test.dart
@@ -1,8 +1,3 @@
-import 'package:new_app/new_app.dart';
+import 'package:new_app/Funcionario.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
