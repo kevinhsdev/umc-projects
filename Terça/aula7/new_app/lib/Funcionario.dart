4c9464a290830cbfeab1e5530c574f1a32a77d9a
kevinhsdev <kevinhs07@outlook.com>
2026-09-22 08:58:40 -0300

    fix names & add new function

--- Terça/aula7/new_app/lib/Funcionario.dart
+++ Terça/aula7/new_app/lib/Funcionario.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+abstract class Funcionario {
+  String _nome = "";
+  double _salario = 0.0;
+
+  //Construtor
+  Funcionario(String nome, double salario) {
+    this.setNome = nome;
+    this.setSalario = salario;
+  }
+
+  //getters & setters
+
+  String get getNome {
+    return _nome;
+  }
+
+  double get getSalario {
+    return _salario;
+  }
+
+  void set setNome(String novoNome) {
+    if (novoNome.isEmpty) {
+      throw Exception("Nome não pode ser vazio");
+    }
+    this._nome = novoNome;
+  }
+
+  void set setSalario(double novoSalario) {
+    if (novoSalario < 0) {
+      throw Exception("Salário não pode ser negativo");
+    }
+    this._salario = novoSalario;
+  }
+  
+  //exibir informações do funcionário
+  String exibirInformacoes(){
+    return("Nome: $getNome, Salário: $getSalario");
+  }
 }
